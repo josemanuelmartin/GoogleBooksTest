f71b2fe1c4d0d4a013fd6ec4622f0ecce3d8405b
Jose Manuel Martín <martingonzalezjosemanuel@gmail.com>
2018-03-07 00:12:14 +0100

    CustomSearchBar

---
+++ GoogleBooks/Helpers/Components/Search/CustomSearchBarDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  CustomSearchBar.swift
+//  GoogleBooks
+//
+//  Created by Jose Manuel Martín González.
+//  Copyright © 2018 martingonzalezjosemanuel. All rights reserved.
+//
+
+protocol CustomSearchBarDelegate {
+    func changeDisposition()
+}
