144c0343d8c07d5d6245e0aa926f74371b178cc8
Jose Manuel Martín <martingonzalezjosemanuel@gmail.com>
2018-03-07 00:08:56 +0100

    Protocols

---
+++ GoogleBooks/Domain/Core/Protocols/Presenter.swift
@@ -0,0 +1,31 @@
+//
+//  Presenter.swift
+//  GoogleBooks
+//
+//  Created by Jose Manuel Martín González.
+//  Copyright © 2018 martingonzalezjosemanuel. All rights reserved.
+//
+
+import Foundation
+
+protocol Presenter {
+    
+    func viewDidLoad()
+    func viewWillAppear(animated: Bool)
+    func viewDidAppear(animated: Bool)
+    func viewWillDisappear(animated: Bool)
+    func viewDidDisappear(animated: Bool)
+    
+    func didReceiveMemoryWarning()
+}
+
+extension Presenter {
+    
+    func viewDidLoad() {}
+    func viewWillAppear(animated: Bool) {}
+    func viewDidAppear(animated: Bool) {}
+    func viewWillDisappear(animated: Bool) {}
+    func viewDidDisappear(animated: Bool) {}
+    
+    func didReceiveMemoryWarning() {}
+}
---
+++ GoogleBooks/Domain/Core/Protocols/Wireframe.swift
@@ -0,0 +1,13 @@
+//
+//  Wireframe.swift
+//  GoogleBooks
+//
+//  Created by Jose Manuel Martín González.
+//  Copyright © 2018 martingonzalezjosemanuel. All rights reserved.
+//
+
+import Foundation
+
+protocol Wireframe {
+    
+}
