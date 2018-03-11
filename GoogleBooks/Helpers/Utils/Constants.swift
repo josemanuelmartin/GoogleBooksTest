277dd8c83cfdf859ef96cc6bea29f6b563d2bbab
Jose Manuel Martín <martingonzalezjosemanuel@gmail.com>
2018-03-12 00:37:25 +0100

    Constants file

---
+++ GoogleBooks/Helpers/Utils/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  GoogleBooks
+//
+//  Created by Jose Manuel Martín González.
+//  Copyright © 2018 martingonzalezjosemanuel. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct url {
+        static let baseEndpoint = "https://www.googleapis.com/books/v1/"
+        static let volumes = "volumes?q="
+    }
+}
