3972fae3d7ab146f22d244aca064459295d363de
Jose Manuel Martín <martingonzalezjosemanuel@gmail.com>
2018-03-11 14:53:37 +0100

    Clean code / Refactor

--- GoogleBooks/Helpers/Extensions/UIColorExtensions.swift
+++ GoogleBooks/Helpers/Extensions/UIColorExtensions.swift
@@ -11,11 +11,17 @@ import UIKit
 extension UIColor {
     
     static var customGray: UIColor {
-        return UIColor(red: 155.0 / 255.0, green: 155.0 / 255.0, blue: 155.0 / 255.0, alpha: 1)
+        return UIColor(red: 155.0 / 255.0,
+                       green: 155.0 / 255.0,
+                       blue: 155.0 / 255.0,
+                       alpha: 1)
     }
     
     static var customDarkGray: UIColor {
-        return UIColor(red: 74.0 / 255.0, green: 74.0 / 255.0, blue: 74.0 / 255.0, alpha: 1)
+        return UIColor(red: 74.0 / 255.0,
+                       green: 74.0 / 255.0,
+                       blue: 74.0 / 255.0,
+                       alpha: 1)
     }
     
 }
